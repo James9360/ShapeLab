60f5f5bbec1f1c76c14e120acb814fec4c21e269
peter losey <plosey1991@gmail.com>
2016-02-24 19:50:57 -0500

    Shape interface and shape methods v1

--- src/edu/cis232/shape/Circle.java
+++ src/edu/cis232/shape/Circle.java
@@ -1,6 +1,16 @@
 package edu.cis232.shape;
 
-public class Circle 
+public class Circle implements Shape
 {
-
+	double v1 = 0;
+	public Circle(double value){
+		v1 = value;	
+	}
+	final double PI = Math.PI;
+	public double getArea(){
+		return v1 * PI * PI;
+	}
+	public double getPerimeter(){
+		return 2 * PI * v1;
+	}
 }
--- src/edu/cis232/shape/Rectangle.java
+++ src/edu/cis232/shape/Rectangle.java
@@ -2,5 +2,16 @@ package edu.cis232.shape;
 
 public class Rectangle 
 {
-
+	double v1 = 0;
+	double v2 = 0;
+	public Rectangle(double value1, double value2){
+		v1 = value1;
+		v2 = value2;	
+	}
+	public double getArea(){
+		return v1*v2;
+	}
+	public double getPerimeter(){
+		return v1+v1+v2+v2;
+	}
 }
